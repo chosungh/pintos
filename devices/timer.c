278bf837512a7caaa14445988bb548b166b6b519
hyoyoyh <danmuji4049@gmail.com>
2025-11-10 19:54:33 +0900

    성현의 alarm merge

--- devices/timer.c
+++ devices/timer.c
@@ -3,11 +3,13 @@
 #include <inttypes.h>
 #include <round.h>
 #include <stdio.h>
+#include <string.h>
+#include "list.h"
+#include "stdbool.h"
 #include "threads/interrupt.h"
 #include "threads/io.h"
 #include "threads/synch.h"
 #include "threads/thread.h"
-#include "list.h"
 
 /* See [8254] for hardware details of the 8254 timer chip. */
 
@@ -17,9 +19,12 @@
 #if TIMER_FREQ > 1000
 #error TIMER_FREQ <= 1000 recommended
 #endif
-static struct list sleep_list;
+
 /* Number of timer ticks since OS booted. */
+// OS가 부팅된 이후 타이머 틱 수
 static int64_t ticks;
+
+static struct list sleep_list;
 
 /* Number of loops per timer tick.
    Initialized by timer_calibrate(). */
@@ -29,10 +34,27 @@ static intr_handler_func timer_interrupt;
 static bool too_many_loops (unsigned loops);
 static void busy_wait (int64_t loops);
 static void real_time_sleep (int64_t num, int32_t denom);
+static void thread_wake_up(void);
+static bool sleep_list_less(const struct list_elem *a, const struct list_elem *b, void *aux);
+
+bool sleep_list_less(const struct list_elem *a, const struct list_elem *b, void *aux) {
+	const struct thread *t1 = list_entry(a, struct thread, elem);
+	const struct thread *t2 = list_entry(b, struct thread, elem);
+
+	return t1->wakeTime < t2->wakeTime;
+};
+
+
+
 
 /* Sets up the 8254 Programmable Interval Timer (PIT) to
    interrupt PIT_FREQ times per second, and registers the
    corresponding interrupt. */
+
+/*
+8254 타이머 하드웨어를 설정해서 초당 PIT_PREQ번 인터럽트를 발생시키고, 
+그 인터럽트를 처리할 함수를 OS에 등록한다. 
+*/
 void
 timer_init (void) {
 	/* 8254 input frequency divided by TIMER_FREQ, rounded to
@@ -42,6 +64,7 @@ timer_init (void) {
 	outb (0x43, 0x34);    /* CW: counter 0, LSB then MSB, mode 2, binary. */
 	outb (0x40, count & 0xff);
 	outb (0x40, count >> 8);
+	
 
 	intr_register_ext (0x20, timer_interrupt, "8254 Timer");
 }
@@ -72,40 +95,58 @@ timer_calibrate (void) {
 }
 
 /* Returns the number of timer ticks since the OS booted. */
+// OS가 부팅된 이후 타이머 틱의 수를 반환하는 함수?
 int64_t
 timer_ticks (void) {
-	enum intr_level old_level = intr_disable ();
-	int64_t t = ticks;
-	intr_set_level (old_level);
-	barrier ();
+	enum intr_level old_level = intr_disable (); //intr_disable(): 현재 인터럽트를 비활성하고 이전 인터럽트 상태를 반환
+	int64_t t = ticks; 
+	intr_set_level (old_level); // 이전 인터럽트 상태를 반환
+	barrier (); 
 	return t;
 }
 
 /* Returns the number of timer ticks elapsed since THEN, which
    should be a value once returned by timer_ticks(). */
+
+// 프로그램(OS이지 않을까?)이 실행된 이후 흐른 틱 수를 반환하는 함수
 int64_t
 timer_elapsed (int64_t then) {
 	return timer_ticks () - then;
 }
-bool compare_time(const struct list_elem *a, const struct list_elem *b, void *aux UNUSED) {
 
-    struct thread *t1 = list_entry(a, struct thread, elem);
-	struct thread *t2 = list_entry(b, struct thread, elem);
-	bool result = t1->wakeup_time < t2->wakeup_time;
-    return result;
-}
 /* Suspends execution for approximately TICKS timer ticks. */
+
+/*
+현재 문제
+	busy waiting이 발생하고 있음
+	busy waiting: 조건이 만족될 때까지 계속 검사만 하는 것
+
+우리의 목표
+	1. busy waiting을 없애는 것
+	2. 효율적으로 스레드를 재운 뒤 지정된 시간에 깨우는 timer_sleep을 구현하는 것
+
+방법
+	1. 스레드를 깨울 시간을 계산
+	2. 현재 스레드를 재움 어떻게? thread_current()함수를 통해 현재 스레드 확인
+	3. 그리고 스레드 구조체에 wakeuptime을 넘겨서 깨울 시간 설정
+*/
 void
 timer_sleep (int64_t ticks) {
-	int64_t wakeup_time = timer_ticks() + ticks;
+	int64_t WakeUpTime = timer_ticks() + ticks;	// 종료시간 계산
+	struct thread *curr = thread_current();		// 현재 실행 중인 스레드 정보 가져옴	
+	curr->wakeTime = WakeUpTime;	// 현재 실행 중인 스레드의 종료시간을 설정
 	
-	struct thread *t = thread_current();
-
-	t->wakeup_time = wakeup_time;
-	list_insert_ordered(&sleep_list,&t->elem,compare_time,NULL); // 삽입정렬 함수 sleep list에 wakeup time을 이제 오름차순으로 삽입해서 제일 앞에 있는 애 빼서 쓸수있게 그러면 순회안하도됨
-	enum intr_level old_level = intr_disable (); // interupt 를 꺼야 호환성 문제 안생김
-	thread_block(); // sleep  이새끼가 재우는 친구
-	intr_set_level (old_level); // 이건 인터럽트 키는 친구 하던일 멈추고 딴일 시키느거
+	// wakeTime을 비교해서 오름차순 정렬
+	list_insert_ordered(&sleep_list, &curr->elem, sleep_list_less, NULL);
+	
+	// 이후 인터럽트를 disable하고 스레드 blocked 한 다음, 인터럽트 enable
+	enum intr_level old_level = intr_disable (); 
+	thread_block();
+	intr_set_level (old_level);
+	
+	// ASSERT(): 에러 검출용 함수 
+	// intr_get_level(): 인터럽트 활성화 상태를 반환하는 함수
+	// 인터럽트 : CPU가 현재 실행 중인 프로그램을 잠시 멈추고, 긴급하게 처리해야 할 다른 작업을 먼저 처리하게 하는 신호나 메커니즘
 }
 
 /* Suspends execution for approximately MS milliseconds. */
@@ -131,28 +172,28 @@ void
 timer_print_stats (void) {
 	printf ("Timer: %"PRId64" ticks\n", timer_ticks ());
 }
-
+
 /* Timer interrupt handler. */
 static void
 timer_interrupt (struct intr_frame *args UNUSED) {
 	ticks++;
-	thread_tick ();
-	wakeup_thread();
+	thread_tick();
 
+	thread_wake_up();
 }
 
-void
-wakeup_thread(void) {
+void thread_wake_up(void) {
+	struct thread *sleep_thread; 
+
 	while (!list_empty(&sleep_list)) {
-		struct list_elem *poped_list = list_front(&sleep_list);
-		struct thread *thread = list_entry(poped_list,struct thread,elem);
-		if (thread->wakeup_time <= timer_ticks()) {
+		sleep_thread = list_entry(list_front(&sleep_list), struct thread, elem);
+		if (timer_ticks() >= sleep_thread->wakeTime && !list_empty(&sleep_list)) {
 			list_pop_front(&sleep_list);
-			enum intr_level old_level = intr_disable();
-			thread_unblock(thread);
-			intr_set_level(old_level); // 이건 스포임
-		}
-		else {
+	
+			enum intr_level old_level = intr_disable (); 
+			thread_unblock(sleep_thread);
+			intr_set_level (old_level);
+		} else {
 			break;
 		}
 	}
